6df59b08d1a8c15b2fddb10a8b0b21c03537c5a7
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-09-30 17:08:22 +0530

    update: fetch title using html.parse

--- cmd/gofetch/main.go
+++ cmd/gofetch/main.go
@@ -2,27 +2,46 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"os"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
 	var url string = os.Args[1]
 	resp, err := http.Get(url)
-	if resp != nil {
-		defer resp.Body.Close()
-		fmt.Println("Response Code", resp.StatusCode)
-		body, err := io.ReadAll(resp.Body)
 
-		if err != nil {
-			fmt.Println("Body Error: ", err)
-			return
-		}
-
-		fmt.Println(string(body))
-		// fmt.Println(body)
-	} else {
-		fmt.Println("Request Error: ", err)
+	if err != nil {
+		fmt.Println("Request Error", err)
+		return
 	}
+
+	defer resp.Body.Close()
+	fmt.Println("Response Code", resp.StatusCode)
+
+	rootNode, err := html.Parse(resp.Body)
+
+	if err != nil {
+		fmt.Println("Parse Error: ", err)
+		return
+	}
+
+	title := findTitle(rootNode)
+	fmt.Println(title)
+}
+
+func findTitle(node *html.Node) string {
+
+	if node.Type == html.ElementNode && node.Data == "title" {
+		return node.FirstChild.Data
+	} else {
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			result := findTitle(child)
+			if result != "" {
+				return result
+			}
+		}
+	}
+	return ""
 }
