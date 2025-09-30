5a5159ea5624a08fde42c2e3af2720f51bdb9566
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2025-09-30 16:55:24 +0530

    [update] read url, fetch body

--- cmd/gofetch/main.go
+++ cmd/gofetch/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to gofetch")
+	var url string = os.Args[1]
+	resp, err := http.Get(url)
+	if resp != nil {
+		defer resp.Body.Close()
+		fmt.Println("Response Code", resp.StatusCode)
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Println("Body Error: ", err)
+			return
+		}
+
+		fmt.Println(string(body))
+		// fmt.Println(body)
+	} else {
+		fmt.Println("Request Error: ", err)
+	}
 }
